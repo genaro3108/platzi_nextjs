8e4bd1019d4f19369923003a52df4ae7aaecf791
genaro3108 <genaro3108@gmail.com>
2021-05-24 14:55:20 -0500

    contenido estatic

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,35 @@ import React, { useState, useEffect } from 'react'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
+import fetch from 'isomorphic-unfetch'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
+//Server side rendering
+//Bajo demanda, siempre va al API
+/*export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3000/api/avo');
+  const { data: productList }: TAPIAvoResponse = await response.json()
+  return {
+    props: {
+      productList
+    }
+  }
+}*/
+
+//Static generation, tiene un impacto en el performance
+//Solo sucede una vez, en cuanto se genera el paquete de produccion
+export const getStaticProps = async () => {
+  const response = await fetch('http://localhost:3000/api/avo');
+  const { data: productList }: TAPIAvoResponse = await response.json()
+  return {
+    props: {
+      productList
+    }
+  }
+}
+
+
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
 
   return (
     <Layout>
