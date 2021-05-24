b645cce48c17f21d13400ee612a194399aaf3e11
genaro3108 <genaro3108@gmail.com>
2021-05-24 14:54:36 -0500

    contenido estatico

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,24 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-
+import { GetStaticProps } from "next";
 import Layout from '@components/Layout/Layout'
 import ProductSummary from '@components/ProductSummary/ProductSummary'
+import fetch from 'isomorphic-unfetch'
 
-const ProductPage = () => {
-  const { query } = useRouter()
-  const [product, setProduct] = useState<TProduct | null>(null)
 
-  useEffect(() => {
-    if (query.id) {
-      window
-        .fetch(`/api/avo/${query.id}`)
-        .then((response) => response.json())
-        .then((data: TProduct) => {
-          setProduct(data)
-        })
+export const getStaticPaths = async () => {
+  const response = await fetch('http://localhost:3000/api/avo');
+  const { data: productList }: TAPIAvoResponse = await response.json()
+  const paths = productList.map(({ id }) => ({ params: { id } }));
+
+  return {
+    paths,
+    //fallback
+    //Incremental statci generation
+    //En false, da 404 para cada pagina que no este en los paths
+    fallback:false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params?.id as string;
+  const response = await fetch(`http://localhost:3000/api/avo/${id}`);
+  return {
+    props: {
+      product
     }
-  }, [query.id])
+  }
+}
 
+const ProductPage = ({ product }: { product: TProduct }) => {
   return (
     <Layout>
       {product == null ? null : <ProductSummary product={product} />}
